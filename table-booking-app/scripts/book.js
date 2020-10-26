bf4799dcab5e64021e3431fb4cb6eebb6618a4b8
Faris Taha <cyber.yukarin@gmail.com>
2020-10-26 21:22:47 +0800

    add id to book.html and event listener to book.js

---
+++ table-booking-app/scripts/book.js
@@ -0,0 +1,33 @@
+function BookNow(guestName,guestEmail,guestPax){
+    let url = 'https://api.sheety.co/52fd51b7982eaf677da56f9095c77e38/bookingApp/bookings';
+    let body = {
+      booking: {
+        name:guestName,
+        email:guestEmail,
+        pax:guestPax,
+      }
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type':'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with object
+      console.log(json.booking);
+      document.getElementById("bookMsg").innerHTML = json.booking.name + " successfully added!";
+      GetBookings();
+    });
+}
+
+document.getElementById("bookNow").addEventListener("click", function(){
+    let name = document.getElementById("guestName").value;
+    let email = document.getElementById("guestEmail").value;
+    let pax = document.getElementById("guestPax").value;
+    let remarks = document.getElementById("guestRemarks").value;
+
+    console.log(name + ", " + email + ", " + pax + ", " + remarks);
+});
