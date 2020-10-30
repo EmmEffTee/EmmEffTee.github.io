527c47034037df5dcafb7b121d85fd758bf8dc7c
Faris Taha <cyber.yukarin@gmail.com>
2020-10-30 21:31:44 +0800

    Added components

---
+++ vuejs/scripts/components.js
@@ -0,0 +1,16 @@
+Vue.component("todo-item", {
+    props: ["todo"],
+    template: "<li>{{ todo.text }}</li>"
+});
+
+var app7 = new Vue({
+    el: "#app7",
+    data: {
+        groceryList: [
+            { id:0, text: "Vegetables"},
+            { id:1, text: "Cheese"},
+            { id:2, text: "Mutton"},
+            { id:3, text: "Drinks"},
+        ]
+    }
+});
